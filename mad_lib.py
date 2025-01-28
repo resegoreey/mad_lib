4b7864a825434b8e801373c5a60f951c782d88c8
RESEGO MOTLHASI <remotlhjhb024@student.wethinkcode.co.za>
2025-01-29 00:17:20 +0200

    Added a simple code for mad lib generator

---
+++ mad_lib.py
@@ -0,0 +1,7 @@
+name = input("Input a noun: ")
+adjective = input("Input an adjective: ")
+adverb = input("Choose and adverb: ")
+exclamation = input("Input your favorite exclamation: ")
+
+print(f"""Once upon a time, there lived a {adjective} Princess, named {name}
+She was known to walk {adverb} and the whole town goes {exclamation}!""")
